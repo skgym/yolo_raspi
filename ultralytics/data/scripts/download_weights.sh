ac2411afcf02ef5e701f0351e86c463537ce7ee4
UltralyticsAssistant <web@ultralytics.com>
2026-06-22 04:43:13 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/data/scripts/download_weights.sh
+++ ultralytics/data/scripts/download_weights.sh
@@ -8,7 +8,7 @@
 #     ├── yolov8s.pt
 #     └── ...
 
-python - <<EOF
+python - << EOF
 from ultralytics.utils.downloads import attempt_download_asset
 
 assets = [f'yolov8{size}{suffix}.pt' for size in 'nsmlx' for suffix in ('', '-cls', '-seg', '-pose')]
